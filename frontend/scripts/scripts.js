7371a2e942d918c2a171e40e5c17551e86b1df12
Mohammadreza Sadeghi <mohammadrezasadeqi161@gmail.com>
2025-03-10 20:03:17 +0200

    data refresh pop up fixed to show on the right time

--- frontend/scripts/scripts.js
+++ frontend/scripts/scripts.js
@@ -493,7 +493,9 @@ function getMyData(called_from) {
             fillMyDataPage(data);
             document.getElementById('connect-button-container').innerHTML = `Connected to Fitbit device`;
             document.getElementById('refresh-button').disabled = false;
-            alert('Your data got refreshed.');
+            if (called_from === "refresh button") {
+                alert('Your data got refreshed.');
+            }
         })
         .catch(error => {
             if (error.status === 403) {
