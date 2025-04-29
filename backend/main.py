c6e78befd658a303ad729a8816fb98a0b257ac45
Mohammadreza Sadeghi <mohammadrezasadeqi161@gmail.com>
2025-04-29 12:57:29 +0300

    creating studies and returning all public studies enabled, back to normal setup

--- backend/main.py
+++ backend/main.py
@@ -365,11 +365,11 @@ async def create_study(
         request: Request,
         db: Session = Depends(get_db)
 ):
-    # body = await request.json()
-    # data = schemas.StudySchema.parse_obj(body)
-    # user = authentication_utils.get_current_user(request, db)
-    # crud.add_study(db, user.id, data)
-    return "disabled from server"
+    body = await request.json()
+    data = schemas.StudySchema.parse_obj(body)
+    user = authentication_utils.get_current_user(request, db)
+    crud.add_study(db, user.id, data)
+    # return "disabled from server"
 
 
 @app.put("/study/{study_id}/")
@@ -620,8 +620,8 @@ async def get_public_studies(
         pass
     response = []
     for study in public_studies:
-        if study.id == 1: # Temporary code, should remove later
-            response.append(responses.StudyResponseSchema.from_orm(study))
+        # if study.id == 1: # Temporary code, should remove later
+        response.append(responses.StudyResponseSchema.from_orm(study))
 
     return response
 
